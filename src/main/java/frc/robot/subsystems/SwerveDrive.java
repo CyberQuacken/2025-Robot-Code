59f4f8829ed6f3ef4f86acff6a5807f86b612de9
SachinPrabhuTheCoder <thisissachin18@gmail.com>
2025-01-08 17:56:25 -0600

    Update SwerveDrive.java
    All of this was from the Doc

--- src/main/java/frc/robot/subsystems/SwerveDrive.java
+++ src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -5,6 +5,34 @@ package frc.robot.subsystems;
  * for calculating where to drive
  * and sending directions to the SwerveModules
  */
-public class SwerveDrive {
-    
+public class SwerveDrive extends SubsystemBase{
+    //Attributes
+    SwerveDriveKinematics kinematics;
+    SwerveDriveOdometry odometry;
+    Gyroscope gyro;
+    SwerveModule[] swerveModules;
+
+    //Constructor
+    public SwerveDrive()
+    {
+        swerveModules = new SwerveModule[4]; //Creates Swerve Modules
+
+        kinematics = newSwerveDriveKinematics
+        (
+            new Translation2d(Units.inchesToMeters(12.5),Units.inchesToMeters(12.5))
+            new Translation2d(Units.inchesToMeters(12.5),Units.inchesToMeters(-12.5))
+            new Translation2d(Units.inchesToMeters(-12.5),Units.inchesToMeters(12.5))
+            new Translation2d(Units.inchesToMeters(-12.5),Units.inchesToMeters(-12.5))
+        );
+
+        gyro = new Gyroscope();
+
+        odometry = new SwerveDriveOdometry
+        (
+            kinematics
+            gyro.getAngle()
+            new SwerveModulePosition[]{new SwerveModulePosition(),new SwerveModulePosition(),new SwerveModulePosition(),new SwerveModulePosition()}
+            new Pose2d(0,0,new Rotation2d())
+        );
+    }
 }
