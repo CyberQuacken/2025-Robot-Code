bdc1e734716df9ba13e9a5c701a88fc66c44b066
Bryan Kitzrow <quacken1819@gmail.com>
2025-01-08 17:40:23 -0600

    basic placeholders

---
+++ src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems;
+
+/*
+ * This is the swerve drive class
+ * for calculating where to drive
+ * and sending directions to the SwerveModules
+ */
+public class SwerveDrive {
+    
+}
---
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -0,0 +1,28 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+
+/*
+ * This is the module class
+ * for each individual wheel set
+ * including its enconder, the driving motor, and its turning motor
+ */
+public class SwerveModule {
+    //Drive Motor
+    //turning Motor
+
+    //drive encoder
+    //turn encoder
+    
+    //PID
+
+    public void SwerveModule() {
+
+    }
+
+
+    public void setState(SwerveModuleState wantedState) {
+        //drive Motor.set(wantedState.getDrive)
+        //turn motor.set(wantedState.getTurn)
+    }
+}
