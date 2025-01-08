2e52a89fc94dd5dbd929dd4c6a9a0a2b18139531
Bryan Kitzrow <quacken1819@gmail.com>
2025-01-08 17:49:44 -0600

    a little more placeholdrs

--- src/main/java/frc/robot/subsystems/SwerveDrive.java
+++ src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -6,5 +6,30 @@ package frc.robot.subsystems;
  * and sending directions to the SwerveModules
  */
 public class SwerveDrive {
+ /*
+  * SwerveModules[] modules = new ~~~ {
+  * module [0] // front left
+  * module [1] // front right
+  * module [2] // back left
+  * module [3] // back right
+  * }
+  */
+
+  /*
+   * Gyro
+   * kinematice
+   * odometry
+   */
     
+
+    //set up the swerve Drive subsystem
+    public SwerveDrive(){
+
+    }
+
+    // In robot container this is used every second or so
+    // Take inputed values (from controller sticks) and calculates
+    public void drive(){
+
+    }
 }
