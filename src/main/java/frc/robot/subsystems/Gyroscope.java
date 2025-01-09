9e167dbad88edd54fe1afcce4f28842e99fa5fad
Bryan Kitzrow <quacken1819@gmail.com>
2025-01-08 18:28:23 -0600

    corrected problems with merge

---
+++ src/main/java/frc/robot/subsystems/Gyroscope.java
@@ -0,0 +1,5 @@
+package frc.robot.subsystems;
+
+public class Gyroscope {
+// this will be removed, instead we will use the gyroscope class of the physcial gyro
+}
--- src/main/java/frc/robot/subsystems/SwerveDrive.java
+++ src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -1,5 +1,14 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 /*
  * This is the swerve drive class
  * for calculating where to drive
@@ -9,7 +18,7 @@ public class SwerveDrive extends SubsystemBase{
     //Attributes
     SwerveDriveKinematics kinematics;
     SwerveDriveOdometry odometry;
-    Gyroscope gyro;
+    Gyroscope gyro; // dependant on what gyroscope we use
     SwerveModule[] swerveModules;
 
     //Constructor
@@ -17,14 +26,13 @@ public class SwerveDrive extends SubsystemBase{
     {
         swerveModules = new SwerveModule[4]; //Creates Swerve Modules
 
-        kinematics = newSwerveDriveKinematics
+        kinematics = new SwerveDriveKinematics
         (
-            new Translation2d(Units.inchesToMeters(12.5),Units.inchesToMeters(12.5))
-            new Translation2d(Units.inchesToMeters(12.5),Units.inchesToMeters(-12.5))
-            new Translation2d(Units.inchesToMeters(-12.5),Units.inchesToMeters(12.5))
+            new Translation2d(Units.inchesToMeters(12.5),Units.inchesToMeters(12.5)),
+            new Translation2d(Units.inchesToMeters(12.5),Units.inchesToMeters(-12.5)),
+            new Translation2d(Units.inchesToMeters(-12.5),Units.inchesToMeters(12.5)),
             new Translation2d(Units.inchesToMeters(-12.5),Units.inchesToMeters(-12.5))
         );
-
         /*
          * kinematics = new SwerveDriveKinematices (
          * new Translation2d (aka cord points)(units.conversion(Ydistance) , units.conversion(Xdistance))
@@ -35,31 +43,30 @@ public class SwerveDrive extends SubsystemBase{
          * + / - signs are not written with specfics and neither is the x or y accurate to cordnates
          */
 
-         // gyro = new Gyro // setup gyro here, thats about it
-
-         /*
-          * odometry = new SwerveDriveOdometry(
-          * kinematics // i believe this assigns the odometry we are making to kinematics, considering kinematics is the one calculating
-          * gyro.getAngle() // this is pretty useless as periodicly() the odometer will be updated on current gyro angle and swerveModule states
-          * new swerveModulePosition[]{new position, new position, new position, new position} creates a new array for the odometer to use, and fills it will empty positions    
-          * new Pose2d(0,0 ,  new Rotation2d()) // x= 0 , y = 0 , heading 0
-          * // all new object are to fill the odometer with nonexistent values to be filled when updated
-          * );
-          */
-
+        gyro = new Gyroscope();
+        // gyro = new Gyro // setup gyro here, thats about it
+        
+        odometry = new SwerveDriveOdometry
+        (
+            kinematics,
+            gyro.getAngle(), // gyro is currently using a false class, as such it will show error until we use the proper class
+            new SwerveModulePosition[]{new SwerveModulePosition(),new SwerveModulePosition(),new SwerveModulePosition(),new SwerveModulePosition()},
+            new Pose2d(0,0,new Rotation2d())
+        );
+        /*
+        * odometry = new SwerveDriveOdometry(
+        * kinematics // i believe this assigns the odometry we are making to kinematics, considering kinematics is the one calculating
+        * gyro.getAngle() // this is pretty useless as periodicly() the odometer will be updated on current gyro angle and swerveModule states
+        * new swerveModulePosition[]{new position, new position, new position, new position} creates a new array for the odometer to use, and fills it will empty positions    
+        * new Pose2d(0,0 ,  new Rotation2d()) // x= 0 , y = 0 , heading 0
+        * // all new object are to fill the odometer with nonexistent values to be filled when updated
+        * );
+        */
     }
 
+    
     // In robot container this is used every second or so
     // Take inputed values (from controller sticks), if drive will be relative to field, and if rate should be limited
     public void drive(){
-        gyro = new Gyroscope();
-
-        odometry = new SwerveDriveOdometry
-        (
-            kinematics
-            gyro.getAngle()
-            new SwerveModulePosition[]{new SwerveModulePosition(),new SwerveModulePosition(),new SwerveModulePosition(),new SwerveModulePosition()}
-            new Pose2d(0,0,new Rotation2d())
-        );
     }
 }
