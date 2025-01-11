738cd08f2005afdf8fcee1b5d883fc0f83fd6a3e
Bryan Kitzrow <quacken1819@gmail.com>
2025-01-11 15:26:58 -0600

    driver ports

---
+++ src/main/java/frc/robot/DriverUI.java
@@ -0,0 +1,11 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class DriverUI {
+    public DriverUI(){
+    }
+    public void execute(){
+        SmartDashboard.putNumber("test", 0);
+    }
+}
