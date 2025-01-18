c92ac85c1250dcd6c5e14abae571bf0f4e1aedc5
SachinPrabhuTheCoder <thisissachin18@gmail.com>
2025-01-18 10:03:24 -0600

    Create LightsSubsystem.java

---
+++ src/main/java/frc/robot/subsystems/LightsSubsystem.java
@@ -0,0 +1,22 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LightsSubsystem extends ExampleSubsystem
+{
+
+    final Spark lights;//channel?
+    
+    lightSubsytem(int lightsPort){
+        lights = new Spark(0);
+    }
+
+    public void setColor(int pulseWidth){
+        lights.set(pulseWidth);//conversions
+    }
+    public void turnLightsOff(){
+        lights.set(0);
+    }
+    
+}
