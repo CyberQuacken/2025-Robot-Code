e9250ffc8b132001d0d429688bf6681653c9f144
fdlacher <122589285+fdlacher@users.noreply.github.com>
2025-03-12 16:50:46 -0500

    Quick fix
    I made a silly mistake

--- src/main/java/frc/robot/commands/CoralFeederCommands/pathPlannerCoral.java
+++ src/main/java/frc/robot/commands/CoralFeederCommands/pathPlannerCoral.java
@@ -2,6 +2,7 @@ package frc.robot.commands.CoralFeederCommands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Constants.coralFeederConstants;
 import frc.robot.subsystems.CoralFeederSubsystem;
 
 public class pathPlannerCoral extends Command{
@@ -15,7 +16,7 @@ public class pathPlannerCoral extends Command{
 
     @Override
     public void execute(){
-        coralFeeder.outakeMotor();
+        coralFeeder.testMotor(coralFeederConstants.clawIntakeSpeed);
         new WaitCommand(1);//TODO: Mess around with this number
         coralFeeder.stopMotor();
     }
